2ddeb30f97d6c3ab2367b62ca5af0f297352bb97
Stijn Tintel <stijn@linux-ipv6.be>
2026-08-21 18:58:22 +0300

    realtek: rtl83xx: support per-port stack addresses
    Remove the assumption that every follower user port has one common MAC
    address. Report each address through GET_PORT_STATE and have the leader
    query every advertised port before creating its replacement netdevice.
    
    Track a deduplicated set of follower host addresses for FDB ownership,
    so shared addresses are moved once while unique addresses retain their
    individual destinations. Bump the private and generic netlink protocol
    versions to reject peers and clients with the old address semantics.
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- target/linux/realtek/files-6.18/drivers/net/dsa/rtl83xx/rtl-otto.h
+++ target/linux/realtek/files-6.18/drivers/net/dsa/rtl83xx/rtl-otto.h
@@ -1344,7 +1344,12 @@ struct rtl931x_stack_peer_switch_info {
 	u8 cpu_port;
 	u8 stack_port;
 	u8 protocol_version;
-	u8 port_mac[ETH_ALEN];
+};
+
+struct rtl931x_stack_peer_port_info {
+	u32 mtu;
+	u8 flags;
+	u8 mac[ETH_ALEN];
 };
 
 struct rtl931x_stack_conduit {
@@ -1353,6 +1358,11 @@ struct rtl931x_stack_conduit {
 };
 
 #define RTL931X_STACK_RPC_MAX_BODY_LEN	64
+
+struct rtl931x_stack_host_fdb {
+	u8 addr[ETH_ALEN];
+	bool created;
+};
 
 struct rtl931x_stack_context {
 	struct list_head list;
@@ -1385,7 +1395,7 @@ struct rtl931x_stack_context {
 	u8 talk_pending_peer;
 	u8 talk_pending_local;
 	u8 talk_armed_port;
-	u8 delegated_host_mac[ETH_ALEN];
+	struct rtl931x_stack_host_fdb delegated_hosts[RTL931X_STACK_MAX_PORTS];
 	u32 flags;
 	u32 generation;
 	int ifindex;
@@ -1403,7 +1413,7 @@ struct rtl931x_stack_context {
 	bool talk_pending;
 	bool talk_pending_rpc;
 	bool talk_peer_valid;
-	bool delegated_host_fdb_created;
+	u8 delegated_host_count;
 };
 
 struct rtl83xx_flow {
@@ -1852,6 +1862,9 @@ int rtl931x_stack_host_fdb_remove(struct rtl838x_switch_priv *priv,
 int rtl931x_stack_peer_get_switch_info(struct rtl838x_switch_priv *priv,
 				       struct rtl931x_stack_peer_switch_info *info,
 				       struct netlink_ext_ack *extack);
+int rtl931x_stack_peer_get_port_info(struct rtl838x_switch_priv *priv, u8 port,
+				     struct rtl931x_stack_peer_port_info *info,
+				     struct netlink_ext_ack *extack);
 int rtl931x_stack_reps_set(struct rtl838x_switch_priv *priv, bool enabled,
 			   struct netlink_ext_ack *extack);
 void rtl931x_stack_reps_unregister(struct rtl838x_switch_priv *priv);
